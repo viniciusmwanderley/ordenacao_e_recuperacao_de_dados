28ee342e0a625c8c895884ece0743eab71a139b1
Vinícius M. Wanderley <viniciusmedeiros2571@gmail.com>
2019-07-20 16:55:55 -0300

    problema fib25

--- Scripts/Huffman/huffman.py
+++ Scripts/Huffman/huffman.py
@@ -1,14 +1,8 @@
 from collections import Counter
-from os import path
 import argparse
-import pickle as pkl
 
 parser = argparse.ArgumentParser()
-parser.add_argument('file', help='Arquivo de entrada', nargs='?', default=None)
-parser.add_argument(
-    'pickle_file', help='Arquivo com o objeto', nargs='?', default=None)
-parser.add_argument(
-    '-d', '--decode', help='Usado para realizar o decode', action='store_true')
+parser.add_argument('file', default=None)
 args = parser.parse_args()
 
 
@@ -115,42 +109,20 @@ class HuffmanTree():
 if __name__ == '__main__':
     try:
         # verifica se é uma arquivo
-        if args.file:
-            with open(args.file, encoding='latin1', newline='\n') as in_file:
-                text = in_file.read()
+        with open(args.file, encoding='latin1', newline='\n') as in_file:
+            text = in_file.read()
 
-                # verifica se é para realizar o decode
-                if args.decode:
-                    tree = pkl.load(open(args.pickle_file, 'rb'))
-                    print(tree.decode(text))
+            tree = HuffmanTree()
+            tree.add_text(text)
+            tree.mount()
 
-                else:
-                    # realiza o encode do texto e salva o objeto
-                    tree = HuffmanTree()
-                    tree.add_text(text)
-                    tree.mount()
+            # árvore codificada
+            tree_cod = tree.encode(text)
+            print('tree_cod:', tree_cod)
 
-                    print(tree.encode(text))
+            # árvore decodificada
+            tree_decod = tree.decode(tree_cod)
+            print('tree_decod:', tree_decod)
 
-                    # salva o objeto
-                    pkl.dump(tree, open(
-                        path.splitext(args.file)[-2] + '.code', 'wb'))
-        else:
-            # modo interativo
-            while True:
-                text = input()
-
-                if len(text) > 0:
-                    characters = list(text)
-                    tree = HuffmanTree()
-                    tree.add_text(text)
-                    tree.mount()
-                    # print(tree.dict)
-                    code = tree.encode(text)
-                    print(code)
-                    # print(tree.decode(code))
-
-                else:
-                    raise EOFError
     except EOFError:
         pass
