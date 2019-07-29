b9d70caf8fbfe84b83537c0a4e9622916acc8219
Vinícius M. Wanderley <viniciusmedeiros2571@gmail.com>
2019-07-29 19:26:24 -0300

    Diferença no equal

--- Scripts/Huffman/huffman.py
+++ Scripts/Huffman/huffman.py
@@ -33,44 +33,58 @@ class HuffmanTree():
 
     def add_node(self, node):
         self.nodes.append(node)
+
+        # sort inversely by node value
         self.nodes.sort(key=lambda x: x.value, reverse=True)
 
     def mount(self):
+
         while len(self.nodes) > 1:
 
+            # get left/right nodes from end if list
             node_left = self.nodes[-2]
             node_right = self.nodes[-1]
 
+            # set new node to be father of previus nodes
             node = Node(node_left.label + node_right.label,
                         node_left.value + node_right.value)
-
+            #
             node.left = node_left
             node.right = node_right
 
+            # remove last nodes
             self.nodes = self.nodes[:-2]
 
+            # add new node
             self.add_node(node)
 
+        # save root
         self.head = self.nodes[0]
 
+        # create dict of letters
         self.__mount_dict()
 
+        # create reverse dict
         self.__reverse_dict()
-
-    def __mount_dict(self):
-        self.__nav_tree(self.head, '')
 
     def __nav_tree(self, node, code):
 
+        # leaf node
         if node.left == node.right:
             self.dict[node.label] = code
+
         else:
 
+            # left node
             if node.left:
                 self.__nav_tree(node.left, code + '0')
-
+            # right node
             if node.right:
                 self.__nav_tree(node.right, code + '1')
+
+    def __mount_dict(self):
+
+        self.__nav_tree(self.head, '')
 
     def __reverse_dict(self):
 
@@ -82,6 +96,7 @@ class HuffmanTree():
 
         for letter in list(text):
 
+            # Letter is in dict?
             if self.dict.get(letter):
                 result += self.dict[letter]
             else:
@@ -99,6 +114,7 @@ class HuffmanTree():
         for number in code:
             acc += number
 
+            # Is in dict?
             if self.reversed_dict.get(acc):
                 result += self.reversed_dict[acc]
                 acc = ''
