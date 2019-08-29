d4c324e461b3a219a54a8a01142df1e82037fde2
Vinícius M. Wanderley <viniciusmedeiros2571@gmail.com>
2019-08-29 14:27:17 -0300

    Refatora

--- Scripts/Radix_Count_Sort/radix_and_count_sort.py
+++ Scripts/Radix_Count_Sort/radix_and_count_sort.py
@@ -1,4 +1,6 @@
-# Radix sort in Python
+import time
+
+
 def countingSort(array, place):
     size = len(array)
     output = [0] * size
@@ -48,11 +50,56 @@ def get_values_from_matrix(filename):
 
     return values
 
+# CountSort
 
-file = '../Sort_Tests/num.100000.4.in'
+
+def countSort(a, tam_array):
+    a = a.copy()
+    maximum = max(a)
+    minimum = min(a)
+    # print(maximum, minimum)
+
+    if minimum < 0:
+        for i in range(len(a)):
+            # print(i)
+            a[i] -= minimum
+            if maximum < a[i]:
+                maximum = a[i]
+
+    c = [0] * (maximum + 1)
+    b = [0] * tam_array
+
+    for i in range(len(a)):
+        c[a[i]] = c[a[i]] + 1
+
+    for i in range(len(c) - 1):
+        c[i + 1] += c[i]
+
+    for i in range((len(a) - 1), -1, -1):
+        # print(i)
+
+        b[c[a[i]] - 1] = a[i]
+        c[a[i]] -= 1
+
+    if minimum < 0:
+        for i in range(len(b)):
+            b[i] += minimum
+
+    return b
+
+
+file = '../Sort_Tests/num.1000.1.in'
+
+values = get_values_from_matrix(file)
+tam_array = values.pop()
+start = time.time()
+countSort(values, tam_array)
+print('Count Sort Time(s):', time.time() - start)
+
+file = '../Sort_Tests/num.1000.1.in'
 
 values = get_values_from_matrix(file)
 num_array = values.pop()
-print(len(values))
-
-print(radix_sort2(values))
+start = time.time()
+radix_sort2(values)
+print('Radix Sort Time(s):', time.time() - start)
